65c89f383b2bc7023ce1260ad644cb4db3028869
Vasilis Papageorgiou <vpapageorgiou@gmail.com>
2013-02-27 21:41:31 +0000

    Working solution
    Needs some refactoring...

---
+++ StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public string Add(string input)
+        {
+            var delimiters = new List<string> {",", "\n"};
+            var result = 0;
+
+            CheckForBespokeDelimiter(input, delimiters);
+
+            var numbers = input.Split(delimiters.ToArray(), StringSplitOptions.None);
+
+            CheckForNegatives(numbers);
+
+            foreach(var number in numbers)
+            {
+                int output;
+                Int32.TryParse(number,out output);
+                if (output < 1000)
+                {
+                    result += output;
+                }
+            }
+
+            return result.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private static void CheckForNegatives(IEnumerable<string> numbers)
+        {
+            var negativesList = string.Empty;
+            foreach (var number in numbers)
+            {
+                int output;
+                Int32.TryParse(number, out output);
+                if (output < 0)
+                {
+                    negativesList += output + ",";
+                }
+            }
+
+            if (negativesList.Length > 0)
+            {
+                throw new Exception(string.Concat("Negatives not allowed: ", negativesList.Remove(negativesList.Length - 1, 1)));
+            }
+        }
+
+        private static void CheckForBespokeDelimiter(string input, ICollection<string> delimiters)
+        {
+            if (input.Length <= 2) return;
+            switch (input.Substring(0, 2))
+            {
+                case "//":
+                    if (input.Substring(2, 1) != "[")
+                    {
+                        delimiters.Add(input.Substring(2, 1));
+                    }
+                    else
+                    {
+                        foreach (Match m in Regex.Matches(input, @"(?<=\[)(.*?)(?=\])")) //looks for [ ... ]
+                        {
+                            var startLocation = m.Index;
+                            var endLocation = m.Index + m.Value.Length;
+                            delimiters.Add(input.Substring(startLocation, endLocation - startLocation));
+                        }                       
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculatorKataTests/StringCalculatorTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalculatorKata;
+
+namespace StringCalculatorKataTests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [TestCase("","0")]
+        [TestCase("1", "1")]
+        [TestCase("1,2", "3")]
+        [TestCase("1,2,3,1,5,11", "23")]
+        public void AddWithStringNumbersExpectCorrectResults(string input, string result)
+        {
+            var target = new StringCalculator();
+            Assert.That(target.Add(input), Is.EqualTo(result));
+        }
+
+        [TestCase("\n", "0")]
+        [TestCase("1\n", "1")]
+        [TestCase("1\n2", "3")]
+        [TestCase("1\n2,3,1\n5,11", "23")]
+        public void AddWithStringNumbersAndNewLineExpectCorrectResults(string input, string result)
+        {
+            var target = new StringCalculator();
+            Assert.That(target.Add(input), Is.EqualTo(result));
+        }
+
+        [TestCase("\n", "0")]
+        [TestCase("1\n", "1")]
+        [TestCase("1\n2", "3")]
+        [TestCase("1\n2,3,1\n5,11", "23")]
+        [TestCase("//;1\n2;3,1\n5,11", "23")]
+        public void AddWithStringNumbersAndNewLineAndExtraDelimiterExpectCorrectResults(string input, string result)
+        {
+            var target = new StringCalculator();
+            Assert.That(target.Add(input), Is.EqualTo(result));
+        }
+
+        [TestCase("-1,2", "Negatives not allowed: -1")]
+        [TestCase("2,-4,3,-5", "Negatives not allowed: -4,-5")]
+        public void AddWithStringNumbersAndNegativeNumbersThrows(string input, string message)
+        {
+            var target = new StringCalculator();
+            var ex = Assert.Throws<Exception>(() => target.Add(input));
+            Assert.That(ex.Message, Is.EqualTo(message));
+        }
+
+        [TestCase("1001,2", "2")]
+        [TestCase("1001,2,3", "5")]
+        public void AddWithStringNumbersGreatetThan1000ExpectCorrectResults(string input, string result)
+        {
+            var target = new StringCalculator();
+            Assert.That(target.Add(input), Is.EqualTo(result));
+        }
+
+        [TestCase("//[***]\n1***2***3", "6")]
+        public void AddWithStringNumbersAndNewLineAndExtraDelimiterWithVariableLengthExpectCorrectResults(string input, string result)
+        {
+            var target = new StringCalculator();
+            Assert.That(target.Add(input), Is.EqualTo(result));
+        }
+
+        [TestCase("//[*][%]\n1*2%3", "6")]
+        [TestCase("//[**][%]\n1**2%3", "6")]
+        [TestCase("//[***][%%]\n1***2%%3", "6")]
+        public void AddWithStringNumbersAndNewLineAndExtraDelimiterAndMultipleVariableLengthExpectCorrectResults(string input, string result)
+        {
+            var target = new StringCalculator();
+            Assert.That(target.Add(input), Is.EqualTo(result));
+        }
+
+    }
+}
